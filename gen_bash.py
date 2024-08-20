18d3f27e5a94b82ae80951e43b6c7bbd601fec90
hhnqqq <895228612@qq.com>
2024-08-20 16:03:16 +0800

    修改：修改为使用torchrun或者deepspeed启动程序，而不使用accelerate

--- gen_bash.py
+++ gen_bash.py
@@ -1,13 +1,27 @@
 import os
 import sys
+import socket
 
 CMD = sys.argv[1]
+# CMD = '/home/bingxing2/ailab/scx6mh7/workspace/sacc_beijingcloud/test.py'
+
+# COMMAND = f'''
+# deepspeed --num_nodes {os.environ.get('SLURM_NNODES')} \
+#     --num_gpus {os.environ.get('GPUS_PER_NODE')} \
+#     --master_addr {socket.gethostbyname(os.environ.get('MASTER_ADDR'))} \
+#     --master_port {int(os.environ.get('MASTER_PORT'))} \
+#     --hostfile {os.environ.get('SACC_HOME')}/configs/{os.environ.get('SACC_UUID')}/ds_hosts \
+#     --launcher SLURM \
+#     --no_ssh_check \
+#     {CMD}
+# '''
 
 COMMAND = f'''
-echo $SLURM_NODEID
-accelerate launch  --rdzv_conf=timeout=30,join_timeout=40 \
-    --num_cpu_threads_per_process {os.environ.get('OMP_NUM_THREADS')} \
-    --config_file {os.environ.get('SACC_HOME')}/configs/{os.environ.get('SACC_UUID')}/accelerate_$SLURM_NODEID.json {CMD}
+torchrun  --nnodes={os.environ.get('SLURM_NNODES')} \
+    --nproc-per-node={os.environ.get('SLURM_GPUS_ON_NODE')} \
+    --rdzv-endpoint={os.environ.get('MASTER_ADDR')}:${os.environ.get('MASTER_PORT')} \
+    --rdzv-backend=c10d \
+    {CMD}
 '''
 
 run_sh_path = f"{os.environ.get('SACC_HOME')}/configs/{os.environ.get('SACC_UUID')}/run.sh"
--- sacc.py
+++ sacc.py
@@ -1,29 +1,19 @@
 # 多节点训练的主程序
+#!/home/bingxing2/ailab/scx6mh7/peng_tmp_test/miniconda3/envs/hhn/bin/python
 import sys
 import os
 import click
-import json
 import time
 
-from MyTransformers.common.utils.functional_tools import ensure_directory_exists
+from common.utils.functional_tools import ensure_directory_exists
 
 uuid = str(time.time())
-# JOB_ID_r = os.popen("echo ${SLURM_JOB_ID}")
-# JOB_ID 
 
 srcipt_path = os.path.split(os.path.realpath(__file__))[0]
 configs_path = os.path.join(srcipt_path, "configs")
 ensure_directory_exists(configs_path)
 ensure_directory_exists(os.path.join(configs_path, uuid))
-
-
-def set_hpz_param(zero,gpu_per_nodes):
-    deepspeed_config_file = srcipt_path + f'/.deepspeed_zero{zero}.json'
-    target_config_file = srcipt_path + f'/configs/{uuid}/.deepspeed.json'
-    with open(deepspeed_config_file, 'r') as f:
-        deepspeed_config = json.load(f)
-    with open(target_config_file, 'x') as f:
-        json.dump(deepspeed_config, f, indent=4)
+ensure_directory_exists(os.path.join(srcipt_path, 'logs'))
 
 # ------------配置命令行参数，设置了默认值方便使用-----------------
 @click.command(context_settings=dict(
@@ -35,14 +25,13 @@ def set_hpz_param(zero,gpu_per_nodes):
 @click.option('--zero', type=int, default=2, help='Stage of Deepspeed Zero++')
 @click.option('--partition', type=str, default="vip_gpu_ailab", help='partition name')
 @click.option('--group', type=str, default="ai4bio", help='job name')
-@click.option('--conda_env', type=str, default="base", help='conda environment')
-@click.option('--workspace', type=str, default="/home/bingxing2/ailab/hehaonan/workspace/", help='main program dir')
+@click.option('--conda_env', type=str, default="hhn", help='conda environment')
+@click.option('--workspace', type=str, default="/home/bingxing2/ailab/scx6mh7/workspace/MyTransformers", help='main program dir')
 @click.argument('**kwargs', nargs=-1, type=click.UNPROCESSED)
 def main(num_nodes, gpu_per_nodes, job_name,zero,partition,group, conda_env, workspace, **kwargs):
-    set_hpz_param(zero,gpu_per_nodes)
     CMD_START = -1
     for i,j in enumerate(sys.argv[1:]):
-        if(j.endswith('.py')):
+        if(j.endswith('train.py')):
             CMD_START = i
             break
     CMD = " ".join(sys.argv[CMD_START+1:])
@@ -55,7 +44,6 @@ def main(num_nodes, gpu_per_nodes, job_name,zero,partition,group, conda_env, wor
 #SBATCH --gres=gpu:{gpu_per_nodes}
 #SBATCH -p {partition}
 #SBATCH -A {group}
-##SBATCH -w SH-IDC1-10-140-24-123
 
 #SBATCH --output={srcipt_path}/logs/%j.out
 #SBATCH --error={srcipt_path}/logs/%j.err
@@ -64,20 +52,12 @@ export NCCL_ALGO=Ring #NCCL通信算法
 export NCCL_MAX_NCHANNELS=16 #NCCL最大通道数
 export NCCL_MIN_NCHANNELS=16 #NCCL最小通道数
 export NCCL_DEBUG=INFO
+export NCCL_DEBUG_SUBSYS=ALL
 export NCCL_TOPO_FILE=/home/bingxing2/apps/nccl/conf/dump.xml #NCCL拓扑文件路径
 export NCCL_IB_HCA=mx5_0,mlx5_2
 export NCCL_IB_GID_INDEX=3
 export NCCL_IB_TIMEOUT=23 # InfiniBand通信的超时时间
 export NCCL_IB_RETRY_CNT=7 # InfiniBand通信的重试次数
-
-# export NCCL_DEBUG=INFO
-# export NCCL_IB_DISABLE=0
-# export NCCL_IB_HCA=mlx5_bond_0
-# export NCCL_SOCKET_IFNAME=bond0
-# export NCCL_IB_GID_INDEX=3
-
-export WANDB_MODE=online
-export PYTHONUNBUFFERED=1
 
 export PYTHONFAULTHANDLER=1
 export TORCH_CPP_LOG_LEVEL=INFO
@@ -87,18 +67,8 @@ export TORCH_CUDNN_ENABLED=1
 export TORCH_DISTRIBUTED_CUDNN_BENCHMARK=1
 export TORCH_CUDNN_BENCHMARK=1
 
-export CUDA_LAUNCH_BLOCKING=0
-export OMPI_MCA_mtl_base_verbose=1
-export OMP_DYNAMIC=TRUE
-export OMP_NUM_THREADS=2
-
-export HF_HOME=/home/bingxing2/ailab/group/ai4bio/cache
-# export HF_DATASETS_OFFLINE=1
-# export TRANSFORMERS_OFFLINE=1
-# export TORCH_EXTENSIONS_DIR=/HOME/scw6c7z/run/liuwei/cache/torch_extensions/
-
-
-export ZERO_STAGE={zero}
+export SLURM_NNODES={num_nodes}
+export GPUS_PER_NODE={gpu_per_nodes}
 export SACC_HOME={srcipt_path}
 export SACC_UUID={uuid}
 echo $SACC_HOME
@@ -107,35 +77,33 @@ export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export COUNT_NODE=`scontrol show hostnames "$SLURM_JOB_NODELIST" | wc -l`
 export MASTER_PORT=$(python $SACC_HOME/get_good_port.py) # 调用get_good_port.py获取ds主端口，直接使用16666也可以的
 export GRADIO_SERVER_PORT=10046
+export RANK=$SLURM_PROCID
+export GROUP_RANK=$SLURM_NODEID
+export WORLD_SIZE=`expr $SLURM_NNODES * $SLURM_GPUS_ON_NODE`
 
+echo "master addr: $MASTER_ADDR"
 
-
-export CUDA_HOME='/home/bingxing2/apps/compilers/cuda/cuda-12.1'
-# export http_proxy=http://u-cEoRwn:EDvFuZTe@172.16.4.9:3128
-# export https_proxy=http://u-cEoRwn:EDvFuZTe@172.16.4.9:3128
-export HF_ENDPOINT=https://hf-mirror.com
-export HF_HUB_ENABLE_HF_TRANSFER=1
-
-module load anaconda/2021.11
 module load compilers/cuda/12.1
-module load cudnn/8.8.1.3_cuda12.x
-module load compilers/gcc/9.3.0
-
+module load nccl/2.18.3-1_cuda12.1
+module load compilers/gcc/12.2.0
+module load cudnn/8.9.5.29_cuda12.x 
+module load tensorboard/2.11.2   
+module load anaconda/2021.11
 source activate {conda_env}
 
 # 配置ds多节点地址
 srun python $SACC_HOME/gen_ds_hosts.py
-# 配置acclerate
-srun python $SACC_HOME/gen_accelerate.py
-# srun cat /mnt/cache/Chemllm/accelerate_$SLURM_NODEID.json
 
 CMD="{CMD}"
 
 python $SACC_HOME/gen_bash.py "$CMD"
 
 
-# cd {workspace}
-
+echo "Start time: `date`"
+echo "SLURM_JOB_ID: $SLURM_JOB_ID"
+echo "SLURM_NNODES: $SLURM_NNODES"
+echo "SLURM_TASKS_PER_NODE: $SLURM_TASKS_PER_NODE"
+echo "SLURM_NTASKS: $SLURM_NTASKS"
 echo $SLURM_JOB_NAME
 echo $CMD > $SACC_HOME/logs/$SLURM_JOB_ID.cmd.log
 
