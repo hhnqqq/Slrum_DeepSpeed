d3c10056bb84e8842bd70777ca37203b868f256f
hhnqqq <895228612@qq.com>
2024-09-20 11:29:54 +0800

    对齐代码

--- sacc.py
+++ sacc.py
@@ -1,11 +1,21 @@
 # 多节点训练的主程序
 #!/home/bingxing2/ailab/scx6mh7/peng_tmp_test/miniconda3/envs/hhn/bin/python
+
 import sys
 import os
 import click
 import time
+import re
 
 from common.utils.functional_tools import ensure_directory_exists
+
+CMD_START = -1
+for i,j in enumerate(sys.argv[1:]):
+    if(j.endswith('train.py')):
+        CMD_START = i
+        break
+CMD = " ".join(sys.argv[CMD_START+1:])
+experiment_name = re.search(r'.*?--experiment-name\s(.*?)\s.*', CMD).group(1)
 
 uuid = str(time.time())
 
@@ -15,11 +25,71 @@ ensure_directory_exists(configs_path)
 ensure_directory_exists(os.path.join(configs_path, uuid))
 ensure_directory_exists(os.path.join(srcipt_path, 'logs'))
 
+
+def monitor_log_for_errors(script_path, experiment_name):
+    global CMD
+    log_file = os.path.join(script_path, 'logs', f'{experiment_name}.err')
+    communication_error_keywords = ["NCCL", "timeout", "retry count exceeded"]
+
+    while True:
+        # Moniter every 60 seconds.
+        time.sleep(60)  
+        try:
+            with open(log_file, 'r') as log:
+                log_content = log.read()
+                if any(keyword in log_content for keyword in communication_error_keywords):
+                    print("Detected communication error, restarting the training job...")
+                    try:
+                        output_path_match = re.search(r'.*--output-path\s(.*?)\s.*', CMD)
+                        if output_path_match:
+                            output_path = output_path_match.group(1)
+                            if os.path.exists(output_path):
+                                max_step = 0
+                                latest_ckpt_name = ""
+
+                                """
+                                Find checkpoint from the borken training procedure.
+                                In MyTransformers:
+                                    checkpoints are neamed as :
+                                        step_100.ckpt
+                                        step_200.ckpt
+                                        step_300.ckpt
+                                        final.ckpt
+                                """
+                                for file_name in os.listdir(output_path):
+                                    pattern = r'step_(\d*?).ckpt'
+                                    if 'final' in file_name:
+                                        latest_ckpt_name = file_name
+                                        break
+                                    elif re.search(pattern, file_name):
+                                        step = int(re.search(pattern, file_name).group(1))
+                                        if step > max_step:
+                                            max_step = step
+                                            latest_ckpt_name = file_name
+                                
+                                if latest_ckpt_name:
+                                    print(f'Last checkpoint found!: {latest_ckpt_name}')
+                                    CMD += f' --partial-ckpt-path {latest_ckpt_name}'
+                                else:
+                                    print('last checkpoint not found')
+                    except Exception as e:
+                        print(f"Error while processing checkpoints: {e}")
+
+                    print('restart the training procedure!')
+                    os.system(f'sbatch {srcipt_path}/configs/{uuid}/run_slurm.sh > {srcipt_path}/logs/${experiment_name}.log')
+                    os.system(f'parajobs')
+                    break
+        except FileNotFoundError:
+            print(f"Log file {log_file} not found, retrying...")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
 # ------------配置命令行参数，设置了默认值方便使用-----------------
 @click.command(context_settings=dict(
     ignore_unknown_options=True,
 ))
-@click.option('--num_nodes', type=int, default=1, help='Number of nodes')
+@click.option('--num_nodes', type=int, default=2, help='Number of nodes')
 @click.option('--gpu_per_nodes', type=int, default=4, help='Number of GPUs per node')
 @click.option('--job_name', type=str, default=f"{os.environ.get('USER')}_job", help='job name')
 @click.option('--zero', type=int, default=2, help='Stage of Deepspeed Zero++')
@@ -28,14 +98,7 @@ ensure_directory_exists(os.path.join(srcipt_path, 'logs'))
 @click.option('--conda_env', type=str, default="hhn", help='conda environment')
 @click.option('--workspace', type=str, default="/home/bingxing2/ailab/scx6mh7/workspace/MyTransformers", help='main program dir')
 @click.argument('**kwargs', nargs=-1, type=click.UNPROCESSED)
-def main(num_nodes, gpu_per_nodes, job_name,zero,partition,group, conda_env, workspace, **kwargs):
-    CMD_START = -1
-    for i,j in enumerate(sys.argv[1:]):
-        if(j.endswith('train.py')):
-            CMD_START = i
-            break
-    CMD = " ".join(sys.argv[CMD_START+1:])
-
+def main(num_nodes, gpu_per_nodes, job_name,partition,group, conda_env, **kwargs):
 # -------------------要运行的bash命令-------------------------
     BASHCOMMAND = f'''#!/bin/bash
 #SBATCH --job-name={job_name}
@@ -44,14 +107,14 @@ def main(num_nodes, gpu_per_nodes, job_name,zero,partition,group, conda_env, wor
 #SBATCH --gres=gpu:{gpu_per_nodes}
 #SBATCH -p {partition}
 #SBATCH -A {group}
-
-#SBATCH --output={srcipt_path}/logs/%j.out
-#SBATCH --error={srcipt_path}/logs/%j.err
+#SBATCH --cpus-per-gpu=32 
+#SBATCH --output={srcipt_path}/logs/{experiment_name}.out
+#SBATCH --error={srcipt_path}/logs/{experiment_name}.err
 
 export NCCL_ALGO=Ring #NCCL通信算法
 export NCCL_MAX_NCHANNELS=16 #NCCL最大通道数
 export NCCL_MIN_NCHANNELS=16 #NCCL最小通道数
-export NCCL_DEBUG=INFO
+# export NCCL_DEBUG=INFO
 export NCCL_DEBUG_SUBSYS=ALL
 export NCCL_TOPO_FILE=/home/bingxing2/apps/nccl/conf/dump.xml #NCCL拓扑文件路径
 export NCCL_IB_HCA=mx5_0,mlx5_2
@@ -60,8 +123,8 @@ export NCCL_IB_TIMEOUT=23 # InfiniBand通信的超时时间
 export NCCL_IB_RETRY_CNT=7 # InfiniBand通信的重试次数
 
 export PYTHONFAULTHANDLER=1
-export TORCH_CPP_LOG_LEVEL=INFO
-export TORCH_DISTRIBUTED_DEBUG=INFO
+# export TORCH_CPP_LOG_LEVEL=INFO
+# export TORCH_DISTRIBUTED_DEBUG=INFO
 export TORCH_DISTRIBUTED_CUDNN_ENABLED=1
 export TORCH_CUDNN_ENABLED=1
 export TORCH_DISTRIBUTED_CUDNN_BENCHMARK=1
@@ -105,20 +168,28 @@ echo "SLURM_NNODES: $SLURM_NNODES"
 echo "SLURM_TASKS_PER_NODE: $SLURM_TASKS_PER_NODE"
 echo "SLURM_NTASKS: $SLURM_NTASKS"
 echo $SLURM_JOB_NAME
-echo $CMD > $SACC_HOME/logs/$SLURM_JOB_ID.cmd.log
+echo $CMD > $SACC_HOME/logs/{experiment_name}.cmd.log
 
 
-srun bash $SACC_HOME/configs/{uuid}/run.sh > $SACC_HOME/logs/$SLURM_JOB_ID.python.log
-
+srun bash $SACC_HOME/configs/{uuid}/run.sh > $SACC_HOME/logs/{experiment_name}.python.log
 '''
 
     # 将bash命令写入脚本中
     with open(f'{srcipt_path}/configs/{uuid}/run_slurm.sh', 'w') as f:
         f.write(BASHCOMMAND)
 
-    # 将slurm的输出定向到本库的logs文件夹中
-    os.system(f'sbatch {srcipt_path}/configs/{uuid}/run_slurm.sh > {srcipt_path}/logs/$SLURM_JOB_ID.log')
+    # 提交训练任务
+    os.system(f'sbatch {srcipt_path}/configs/{uuid}/run_slurm.sh > {srcipt_path}/logs/{experiment_name}.log')
     os.system(f'parajobs')
+
+    while not os.path.exists(f'{srcipt_path}/logs/{experiment_name}.err'):
+        time.sleep(10)
+        # make sure the file has be created
+    os.system(f'tail -f {srcipt_path}/logs/{experiment_name}.err')
+
+    time.sleep(60)
+    monitor_log_for_errors(srcipt_path, experiment_name)
+
 
 if __name__ == '__main__':
     click.disable_unicode_literals_warning = True
